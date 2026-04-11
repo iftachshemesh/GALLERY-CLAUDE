ae41d922f768b31d9726cc80c43680a9339f8efe
iftachshemesh <doc.ift@gmail.com>
2026-04-11 18:13:11 +0300

    Use CLOUDINARY_URL for config

--- server.js
+++ server.js
@@ -9,11 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 10000;
 
 // ─── Cloudinary config ───────────────────────────────────────────────────────
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key:    process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+cloudinary.config(process.env.CLOUDINARY_URL);
 
 const storage = new CloudinaryStorage({
   cloudinary,
